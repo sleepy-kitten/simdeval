4edde93a2c2785a3f619cc1bb95cb9b32695c3c6
V <V>
2021-12-14 06:53:15 +0100

    add tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,16 +1,25 @@
 #[cfg(test)]
 extern crate test;
 
-use std::mem::{align_of, size_of};
+use std::{mem::{align_of, size_of}, collections::BTreeMap};
+
+use fasteval::Compiler;
 
 use crate::{
-    evaluate::{expression::Expression, function::std::Std, parse_element::ParseElement},
-    functions,
+    evaluate::{
+        enums::Value,
+        expression::Expression,
+        function::{std::Std, Function},
+        node::Node,
+        parse_element::ParseElement,
+        token::Token,
+    },
+    impl_functions,
 };
 
 #[test]
 fn test_parse_fast() {
-    let expression = "1/2+1*3^2+45*43231231541.35252";
+    let expression = "a+43*3-a+b^3";
     let mut expression = Expression::<Std>::new(expression);
     expression.compile().unwrap();
     //let temp = expression;
@@ -34,19 +43,47 @@ fn bench_parse_fast(b: &mut test::Bencher) {
 #[test]
 fn test_fast_sizes() {
     println!("element:   {}", size_of::<ParseElement<Std>>());
+    println!("node:      {}", size_of::<Node<Std>>());
+    println!("token:     {}", size_of::<Token>());
     println!();
     println!("element:   {}", align_of::<ParseElement<Std>>());
 }
+fn root(value: &[Value; 2]) -> Value {
+    todo!()
+}
+fn sqrt(value: &[Value; 1]) -> Value {
+    todo!()
+}
+impl_functions!(Foo: foo; [Std: std]; [Root: root, Sqrt: sqrt]);
+impl_functions!(Bar: bar; [Std: std, Foo: foo]; [Root: root, Sqrt: sqrt]);
+#[test]
+fn test_eval() {
+    let mut expression = Expression::<Bar>::new("3+1+5");
+    expression.compile().unwrap();
+    println!("expression: {:#?}", expression);
+    let result = expression.eval().unwrap();
+    println!("result {:#?}", result);
+}
+#[bench]
+fn bench_eval(b: &mut test::Bencher) {
+    let mut expression = Expression::<Std>::new("2+6^2*4");
+    expression.compile().unwrap();
+    b.iter(||test::black_box(expression.eval()))
+}
 
-functions!(
-    Foo; 
-    bar() -> {Value::Int(12)}; baz() -> {Value::Int(23)}, false);
+#[bench]
+fn bench_eval_fasteval(b: &mut test::Bencher) {
+    use fasteval::Evaler;
+    let parser = fasteval::Parser::new();
+    let mut slab = fasteval::Slab::new();
+    let expression = parser.parse("2+6^2*4", &mut slab.ps).unwrap().from(&slab.ps);
+    let compiled = expression.compile(&slab.ps, &mut slab.cs);
+    b.iter(|| test::black_box(compiled.eval(&slab, &mut fasteval::evalns::EmptyNamespace)))
+}
 
 #[test]
-fn test_macro() {
-    let bar = Foo::bar;
-    let baz = Foo::baz;
-    let test = baz.is_const();
-    let test = Foo::NAMESPACE;
-    println!("test: {}", test);
+fn test_handle_thing() {
+    let mut test = Expression::<Std>::new("a+b+2");
+    let _ = test.compile();
+    let elements = test.elements();
 }
